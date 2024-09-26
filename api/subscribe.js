c61324408474d07f1af97fee613fbd19d4c2eaad
Mary <adebimpecole02@gmail.com>
2024-09-26 06:11:45 +0100

    watch

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,3 +1,4 @@
+let subscriptions = [];
 
 export default function handler(req, res) {
   if (req.method === "POST") {
@@ -12,4 +13,3 @@ export default function handler(req, res) {
     res.status(405).json({ message: "Method not allowed" });
   }
 }
-
