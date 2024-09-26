f5874cac24ef10fd64958a5f59f55c781d6dd6e3
Mary <adebimpecole02@gmail.com>
2024-09-26 08:52:44 +0100

    tt

--- api/vapid-public-key.js
+++ api/vapid-public-key.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   if (req.method === "GET") {
     console.log(process.env.PUBLIC_KEY);
-    res.send(process.env.PUBLIC_KEY);
+    res.send("BIaQhF5lDg7L5pqnXGFh9QN8OT7ymEutB7w7xYo-gM_XZBALLIPL37r4siGYmFIa-E2GWu8ban5mUkSDHthUXuY");
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
