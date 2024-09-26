503acaeaff9423be5842ea3c3b1a524bbafa2918
Mary <adebimpecole02@gmail.com>
2024-09-26 09:35:18 +0100

    mail

--- api/send-notifications.js
+++ api/send-notifications.js
@@ -1,5 +1,19 @@
 const webPush = require("web-push");
 
+// Set VAPID keys
+const vapidKeys = {
+  publicKey:
+    "BIaQhF5lDg7L5pqnXGFh9QN8OT7ymEutB7w7xYo-gM_XZBALLIPL37r4siGYmFIa-E2GWu8ban5mUkSDHthUXuY",
+  privateKey: "h0K6hYWv4ng0nCBzQUio8EpKabG9ZAQR2jUOSdgLD0U",
+};
+
+// Configure web-push with VAPID details
+webPush.setVapidDetails(
+  "mailto:ciacoleman988@gmail.com", // Replace with your email
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
 export default function handler(req, res) {
   if (req.method === "POST") {
     const subscription = req.body.subscription;
@@ -8,7 +22,7 @@ export default function handler(req, res) {
       TTL: 60,
     };
 
-    // Use setTimeout to send notification after the specified delay
+    // Use setInterval to send notification after the specified delay
     setInterval(() => {
       webPush
         .sendNotification(subscription, payload, options)
